0b1ee3b90759f2ee11edaa58b91f948664e8111e
JMinotta <99279568+JMinotta@users.noreply.github.com>
2025-02-09 14:14:35 -0500

    Update solve.java

--- Java/solve.java
+++ Java/solve.java
@@ -29,6 +29,6 @@ public class solve {
     }
 
     public static void main(String[] args) {
-        System.out.println(operacionesMatriciales(100)+" ms");
+        System.out.println("Java: "+operacionesMatriciales(100)+" ms");
     }
 }
